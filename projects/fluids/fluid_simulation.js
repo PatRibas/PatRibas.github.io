41b5be6e6d9f1ea8fb2aa4a8a9308edd011c3ee9
patrick <pwr4470@rit.edu>
2021-04-28 20:54:16 -0400

    Created a 2D plane to draw on and gave it UV values for a texture

--- projects/fluids/fluid_simulation.js
+++ projects/fluids/fluid_simulation.js
@@ -1,22 +1,34 @@
+let mat4 = glMatrix.mat4;
+
 function main() {
     const canvas = document.querySelector("#glCanvas");
     // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+    const gl = canvas.getContext("webgl2");
 
     canvas.width = window.innerWidth; //document.body.clientWidth; 
     canvas.height = window.innerHeight; //document.body.clientHeight; 
 
     // Only continue if WebGL is available and working
     if (gl === null) {
-      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      alert("Unable to initialize WebGL. Your browser or machine may not support it. Reloading the page may fix this error.");
       return;
     }
-  
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // init shaders
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    const programInfo = {
+      program: shaderProgram,
+      attribLocations: {
+        vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        aUV: gl.getAttribLocation(shaderProgram, "aUV"),
+      },
+      uniformLocations: {
+        projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+        modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      },
+    };
+    // draw to window
+    drawScene(gl, programInfo, createPlane(gl));
 }
 
 
@@ -64,6 +76,124 @@ function loadShader(gl, type, source) {
 }
 
 
+function createPlane(gl) {
+
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const positions = [
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  return {
+    position: positionBuffer,
+  };
+}
+
+
+function drawScene(gl, programInfo, buffers) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+  // Clear the canvas before we start drawing on it.
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // Create a perspective matrix, a special matrix that is
+  // used to simulate the distortion of perspective in a camera.
+  // Our field of view is 45 degrees, with a width/height
+  // ratio that matches the display size of the canvas
+  // and we only want to see objects between 0.1 units
+  // and 100 units away from the camera.
+
+  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 10.0;
+  const projectionMatrix = mat4.create();
+
+  // note: glmatrix.js always has the first argument
+  // as the destination to receive the result.
+  mat4.perspective(projectionMatrix,
+                   fieldOfView,
+                   aspect,
+                   zNear,
+                   zFar);
+
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
+
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+                 modelViewMatrix,     // matrix to translate
+                 [0.0, 0.0, -2.43]);  // amount to translate
+
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute.
+  {
+    const numComponents = 2;  // pull out 2 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+                              // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+
+
+    const tex_uv = [
+       0.0,  1.0,
+       1.0,  1.0,
+       0.0,  0.0,
+       1.0,  0.0,
+    ];
+    let uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tex_uv), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(programInfo.attribLocations.aUV);
+    gl.vertexAttribPointer(programInfo.attribLocations.aUV, 2, gl.FLOAT, false, 0, 0);
+}
+
+  // Tell WebGL to use our program when drawing
+
+  gl.useProgram(programInfo.program);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
+}
+
+
+
 window.onload = main;
 
 
@@ -73,19 +203,35 @@ window.onload = main;
  * 
  */
 
-const vsSource = `
-    attribute vec4 aVertexPosition;
+const vsSource = `#version 300 es
+    precision mediump float;
 
+    // uniforms 
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
+    // attributes
+    in vec4 aVertexPosition;
+    in vec2 aUV;
+    out vec2 UV;
+
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      UV = aUV;
     }
 `;
 
-const fsSource = `
+const fsSource = `#version 300 es
+    precision mediump float;
+
+    in vec2 UV;
+    out vec4 fragColor;
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      fragColor = vec4(135.0/255.0, 206.0/255.0, 235.0/255.0, 1.0);
+      float u = UV.s;
+      float v = UV.t;
+      if ( u > 0.5 && v < 0.5 ) {
+        fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      }
     }
   `;
